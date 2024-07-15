85c3390a661d4940fd707de2f1ae3386647a82e0
Loïc Marchand <nilpa018@yahoo.fr>
2024-07-15 14:09:39 +0200

    Class Hello with user entry

--- Hello.java
+++ Hello.java
@@ -1,8 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  *
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Saisissez du texte :");
+        try {
+            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+            String s = bufferRead.readLine();
+            System.out.println(s);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+
+        }
+
     }
 }
